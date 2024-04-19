a0f7efdbc64b593fe991665ee1680a05d5acd198
TomCha <tomas.chabada@seznam.cz>
2024-04-19 14:28:42 +0200

    Přidejte soubory projektu.

---
+++ DBDesignerAndroid/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DBDesignerAndroid
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ DBDesignerAndroid/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DBDesignerAndroid
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
